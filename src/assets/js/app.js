06d4477f2c40dc4effcc9a527055a10873497fdd
Prudkyi <prudkyi87@gmail.com>
2022-10-12 16:24:32 +0200

    Зупинився на замиканнях, функціях. Далі масиви

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,33 @@
 import * as prdkFunctions from "./modules/functions.js";
 
+/* Start */
 prdkFunctions.isWebP();
+
+/* go Work */
+
+'use strict'
+
+function createLid (country) {
+    return function lid (name) {
+        console.log(`${name} - ${country}`)
+    }
+}
+
+let ukr = createLid('Ukraine');
+ukr('Petro');
+ukr('Vasiliy');
+
+console.log(' ')
+
+let pl = createLid('Poland');
+pl('Robert');
+pl('Tomek');
+
+console.log(' ')
+
+let bl = createLid('Belarus');
+bl('Andriy');
+bl('Sergey');
+
 
 
