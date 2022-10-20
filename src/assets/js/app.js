550377c6b164eab3b4b52a43b4c79d55fdb659a8
test_2022 <prudkyi87@gmail.com>
2022-10-20 16:31:26 +0200

    Числа

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,17 +7,24 @@ prdkFunctions.isWebP();
 
 'use strict'
 
-const userInfo = {
-    name: "Vasa",
-    age: 30
+let myNum = 1.005 + Number.EPSILON;
+let numOne = Math.round(myNum * 100) / 100;
+console.log(numOne);
+
+let value = "140.58px";
+console.log(parseFloat(value))
+
+let value2 = 58 + "Frylancer";
+
+if (isNaN(value2)) {
+    console.log("RES NaN")
 }
 
-userInfo.name = "Lena";
+let x = Math.max(10, 20, 30, -5, 1000, 40, 43);
+console.log(x);
 
-console.log(userInfo)
+let y = 12.575;
 
-delete userInfo.name;
-
-console.log(userInfo);
+console.log(Math.floor(y))
 
 
