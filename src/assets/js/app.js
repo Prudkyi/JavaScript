7906ed4fe34121efbd732e048ecffc4854a8d5ed
Prudkyi <prudkyi87@gmail.com>
2022-10-24 16:31:00 +0200

    Вчу DOM

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,24 +7,8 @@ prdkFunctions.isWebP();
 
 'use strict'
 
-let myNum = 1.005 + Number.EPSILON;
-let numOne = Math.round(myNum * 100) / 100;
-console.log(numOne);
+let myElement = document.body;
 
-let value = "140.58px";
-console.log(parseFloat(value))
-
-let value2 = 58 + "Frylancer";
-
-if (isNaN(value2)) {
-    console.log("RES NaN")
-}
-
-let x = Math.max(10, 20, 30, -5, 1000, 40, 43);
-console.log(x);
-
-let y = 12.575;
-
-console.log(Math.floor(y))
-
+console.log(myElement.firstElementChild);
+console.log(myElement.lastElementChild)
 
