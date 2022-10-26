511dff7dcc7b23ee3194bc74480259d2adf1d5d8
test_2022 <prudkyi87@gmail.com>
2022-10-26 05:39:33 +0200

    Ок. Йдемо далі

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,15 +5,10 @@ prdkFunctions.isWebP();
 
 /* go Work */
 
-let main__text = document.querySelector('.main__text');
-let h1 = document.querySelector('#main h1');
+let list = document.querySelector('ul');
+let myLi = list.lastElementChild.innerHTML;
 
-const dubleH1 = h1.cloneNode(true);
-
-main__text.append(dubleH1)
-
-
-console.log(getComputedStyle(h1).fontSize)
+console.log(myLi)
 
 
 
