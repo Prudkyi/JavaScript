973d55fd47b3269983c2304bd8090ba84d4ff442
Prudkyi <prudkyi87@gmail.com>
2022-10-27 13:49:59 +0200

    Вчу собитія

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,12 +4,64 @@ import * as prdkFunctions from "./modules/functions.js";
 prdkFunctions.isWebP();
 
 /* go Work */
-let mainBlock = document.getElementById("main");
+const mainBlock = document.getElementById("main"),
+    contextMenu = document.getElementById("contextMenu");
 
 
-console.log(mainBlock.getBoundingClientRect().top);
+function clickMain (event) {
+    event.preventDefault();
+    let positionX = event.clientX + 10,
+        positionY = event.clientY + 10;
 
-console.log(prdkFunctions.sizeWindow())
+    contextMenu.style.top = `${positionY}px`;
+    contextMenu.style.left = `${positionX}px`;
+    contextMenu.style.display = "inline-block";
+
+    let idElement = "";
+    idElement = event.target.id;
+    function menuEvent (event) {
+
+        console.log(event.target.closest("li").classList)
+
+        event.target.classList.forEach(thisClass => {
+
+            if (thisClass === "contextMenu__redColor") {
+                setColor(idElement, "red");
+                return;
+            }
+            if (thisClass === "contextMenu__greenColor") {
+                setColor(idElement, "green");
+
+            }
+        })
+    }
+
+    contextMenu.addEventListener('click', menuEvent);
+
+}
+
+function closeContextMenu (event) {
+    contextMenu.style.display = "none";
+}
+
+function setColor (element, color) {
+    let el = document.getElementById(element);
+    el.classList = " ";
+    switch (color) {
+        case 'red':
+            el.classList = "redColor";
+            break;
+        case 'green':
+            el.classList = "greenColor";
+            break;
+    }
+
+}
+
+mainBlock.addEventListener("contextmenu", clickMain);
+document.addEventListener("click", closeContextMenu)
+
+
 
 
 
