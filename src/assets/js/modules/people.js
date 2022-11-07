6cdf9d94038c27530582671aa871153303eda4f6
Prudkyi <prudkyi87@gmail.com>
2022-11-07 14:25:15 +0100

    Go Class

---
+++ src/assets/js/modules/people.js
@@ -0,0 +1,14 @@
+class People {
+
+    constructor(fabrik) {
+        this.fabirk = fabrik
+        this.mouth = 0
+    }
+
+    plusMouth () {
+        this.mouth++;
+    }
+
+}
+
+export default People;
