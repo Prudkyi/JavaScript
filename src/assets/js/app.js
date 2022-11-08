17584984bb847abddaeaadd588f7f63f7f7b332e
Prudkyi <prudkyi87@gmail.com>
2022-11-08 13:32:50 +0100

    Go Class and Bind

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,24 +1,6 @@
-import * as prdkFunctions from "./modules/functions.js";
-import MoreArray from "./modules/people.js";
+import Form from "./modules/people.js";
 
-/* Start */
-prdkFunctions.isWebP();
+new Form("contactForm", "nameUser", "emailUser", "phoneUser");
 
-/* lesson */
 
-const myArr = new MoreArray(1,2,3,3,4);
-
-var initialValue = 0;
-var sum = [1,2,3].reduce(
-    (accumulator, currentValue) => {
-        console.log(`${accumulator} ${currentValue}`)
-        return accumulator + currentValue
-    },
-    initialValue
-);
-console.log(sum)
-
-console.log(' ')
-
-myArr.checkAlike()
 
--- src/assets/js/modules/people.js
+++ src/assets/js/modules/people.js
@@ -1,14 +1,28 @@
-class MoreArray extends Array {
+class Form {
 
-    checkAlike() {
-        this.reduce(
-            (acc, element) => {
-                if (acc === element) console.log(false)
-                return element
-            }
-        );
+    constructor (classForm, name, email, phone, text) {
+
+        this.form = document.querySelector(`.contactForm`);
+        this.name = document.querySelector(`.contactForm [name=${name}]`);
+        this.email = document.querySelector(`.contactForm [name=${email}]`);
+        this.phone = document.querySelector(`.contactForm [name=${phone}]`);
+        this.text = document.querySelector(`.contactForm [name=${text}]`);
+        this.submit = document.querySelector(`.contactForm [name=submit]`);
+
+        this.form.addEventListener('submit', this.formSubmit.bind(this));
+
+    }
+
+    checkPhone (el) {
+        return this.phone.value.length >= 12;
+    }
+
+    formSubmit (el) {
+        console.log(this.checkPhone(el))
+        el.preventDefault();
+        console.log('not')
     }
 
 }
 
-export default MoreArray;
+export default Form;
